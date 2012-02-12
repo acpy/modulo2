5eb69e676102f9f4e26fa675afd9969c51df4feb
Luciano Ramalho <luciano@ramalho.org>
2012-02-12 20:08:35 -0200

    memocaras: versao usavel

--- tkinter/memocaras.py
+++ tkinter/memocaras.py
@@ -1,26 +1,33 @@
 # coding: utf-8
 
-from Tkinter import Tk, Canvas, PhotoImage, Frame, NW, HIDDEN
+"""
+Aplicativo para ajudar a memorizar o nome das pessoas em um curso.
+"""
+
+from Tkinter import Tk, Canvas, PhotoImage, Frame, Label
+from Tkinter import HIDDEN, NORMAL, EW, W, NW
 from ttk import Button
 
-posicoes = [
-    [38, 183, 103, 251],
-    [113, 181, 169, 248],
-    [201, 172, 255, 237],
-    [299, 171, 353, 239],
-    [439, 194, 502, 275],
-    [530, 15, 590, 86],
-    [482, 27, 538, 99],
-    [427, 39, 483, 114],
-    [382, 24, 437, 97],
-    [318, 28, 365, 92],
-    [290, 40, 335, 104],
-    [216, 36, 269, 98],
-    [127, 49, 180, 112],
+from random import choice
+
+pessoas = [
+    ('Daniel', [38, 183, 103, 251]),
+    ('Pedro', [113, 181, 169, 248]),
+    ('Leo Bragatti', [201, 172, 255, 237]),
+    ('Flavio', [299, 171, 353, 239]),
+    ('Hugo', [439, 194, 502, 275]),
+    ('Tauan', [530, 15, 590, 86]),
+    ('Luis', [482, 27, 538, 99]),
+    ('Roger', [427, 39, 483, 114]),
+    ('Rodrigo', [382, 24, 437, 97]),
+    ('Danilo', [318, 28, 365, 92]),
+    ('Anderson', [290, 40, 335, 104]),
+    # ('Luciano', [216, 36, 269, 98]),
+    ('Leo Fontolan', [127, 49, 180, 112]),
 ]
 
 
-class MarcaFoto(Frame):
+class MemoCaras(Frame):
 
     def __init__(self, root):
         Frame.__init__(self, root)
@@ -28,26 +35,42 @@ class MarcaFoto(Frame):
         self.canvas = Canvas(self, width=604, height=480)
         self.canvas.grid(column=0, row=0, columnspan=2)
         self.bt_prox = Button(self, text='Próximo', command=self.proximo)
-        self.bt_prox.grid(column=0, row=1)
+        self.bt_prox.grid(column=0, row=1, sticky=W)
+        self.nome = Label(self, text='(passe aqui para ver o nome)')
+        self.nome.bind('<Enter>', self.mostra_nome)
+        self.nome.bind('<Leave>', self.esconde_nome)
+        self.nome.grid(column=1, row=1, sticky=EW)
         nome_arq_foto = 'turma1-640x480.gif'
         self.foto = PhotoImage(file=nome_arq_foto)
         self.canvas.create_image(0, 0, image=self.foto, anchor=NW)
-        self.marca_ativa = None
-        self.caras = []
-        
+        self.caras = {}
+        for nome, bbox in pessoas:
+            marca = self.canvas.create_oval(*bbox, outline='green', width=5,
+                                    state=HIDDEN)
+            self.caras[nome] = marca
+        self.cara_ativa = None
+
     def proximo(self):
-        if self.marca_ativa is None:
-            self.marca_ativa = 0
-        else:
-            self.marca_ativa += 1
-        if self.marca_ativa == len(posicoes):
-            self.marca_ativa = 0
-        self.caras.append(self.canvas.create_oval(*posicoes[self.marca_ativa],
-            outline='green', width=5))
-        if len(self.caras) > 1:
-            self.canvas.itemconfigure(self.caras[-2], state=HIDDEN)    
-        
+        if self.cara_ativa is not None:
+            marca = self.caras[self.cara_ativa]
+            self.canvas.itemconfigure(marca, state=HIDDEN)
+        recente = self.cara_ativa
+        while True:
+            self.cara_ativa = choice(self.caras.keys())
+            if recente != self.cara_ativa:
+                break
+        marca = self.caras[self.cara_ativa]
+        self.canvas.itemconfigure(marca, state=NORMAL)
+
+    def mostra_nome(self, evento):
+        self.texto_nome = self.nome['text']
+        self.nome['text'] = self.cara_ativa
+
+    def esconde_nome(self, evento):
+        self.nome['text'] = self.texto_nome
+
+
 
 root = Tk()
-MarcaFoto(root)
+MemoCaras(root)
 root.mainloop()
