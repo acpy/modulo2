7f80fed4386e5800aa5909e58f9ae94a1428e881
Luciano Ramalho <luciano@ramalho.org>
2012-02-12 05:41:38 -0200

    implementacao do baralho com funcao geradora

---
+++ iteraveis/baralho_gen.py
@@ -0,0 +1,59 @@
+'''
+    >>> b = Baralho()
+    >>> for carta in b:                             # doctest:+ELLIPSIS
+    ...     print carta
+    <A de copas>
+    <2 de copas>
+    <3 de copas>
+    <4 de copas>
+    <5 de copas>
+    ...
+    >>> for carta in reversed(b):                   # doctest:+SKIP
+    ...     print carta
+    <K de paus>
+    <Q de paus>
+    <J de paus>
+    <10 de paus>
+    ...
+    >>>
+
+'''
+
+class Carta(object):
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __repr__(self):
+        return '<%s de %s>' % (self.valor, self.naipe)
+
+class Baralho(object):
+    naipes = 'copas ouros espadas paus'.split()
+    valores = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split()
+
+    def __init__(self):
+        self.cartas = [Carta(v, n)
+                        for n in self.naipes
+                        for v in self.valores]
+
+    def __getitem__(self, pos):
+        raise NotImplementedError()
+        return self.cartas[pos]
+
+    def __len__(self):
+        return len(self.cartas)
+        
+    def __iter__(self):
+        pos = 0
+        while pos < len(self):
+            yield self.cartas[pos]
+            pos += 1
+        
+
+
+
+
+
+
+
+        
